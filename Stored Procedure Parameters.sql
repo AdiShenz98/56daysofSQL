48e7500c190a78bdb7adca87295ccf0c4b1fd8de
Aditya Shenoy <49498445+AdiShenz98@users.noreply.github.com>
2021-05-03 09:11:48 +0530

    Update Stored Procedure Parameters.sql

--- Stored Procedure Parameters.sql
+++ Stored Procedure Parameters.sql
@@ -47,3 +47,21 @@ SELECT @T AS total,
 	   @T2 AS total_in_process;
        
 -- INOUT PARAMETER
+
+DELIMITER //
+
+CREATE PROCEDURE SetCounter(
+	INOUT counter INT,
+    IN inc INT
+)
+BEGIN
+	SET counter = counter + inc;
+END//
+
+DELIMITER ;
+
+SET @counter = 1;
+CALL SetCounter(@counter,1); -- 2
+CALL SetCounter(@counter,1); -- 3
+CALL SetCounter(@counter,5); -- 8
+SELECT @counter; -- 8
