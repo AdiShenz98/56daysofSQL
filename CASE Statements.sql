9fe27444a2f2a01246fa08719101ebcfbeb348fa
Aditya Shenoy <49498445+AdiShenz98@users.noreply.github.com>
2021-05-12 16:21:05 +0530

    Update CASE Statements.sql

--- CASE Statements.sql
+++ CASE Statements.sql
@@ -2,13 +2,26 @@
 
 USE classicmodels;
 
+WITH cte AS (
+	SELECT 
+		customerName, 
+		COUNT(*) orderCount
+	FROM
+		orders
+	INNER JOIN customers 
+		USING (customerNumber)
+	GROUP BY customerName
+)
 SELECT 
     customerName, 
-    COUNT(*) orderCount
+    orderCount,
+    CASE orderCount
+		WHEN 1 THEN 'One-time Customer'
+        WHEN 2 THEN 'Repeated Customer'
+        WHEN 3 THEN 'Frequent Customer'
+        ELSE 'Loyal Customer'
+	end customerType
 FROM
-    orders
-INNER JOIN customers 
-	USING (customerNumber)
-GROUP BY customerName
-ORDER BY COUNT(*);
+    cte
+ORDER BY customerName;
 
