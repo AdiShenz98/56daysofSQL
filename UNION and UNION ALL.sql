7d5b8f88a6c8aa78345b6da2596784f503b5eb0b
Aditya Shenoy <49498445+AdiShenz98@users.noreply.github.com>
2021-05-15 11:09:03 +0530

    Update UNION and UNION ALL.sql

--- UNION and UNION ALL.sql
+++ UNION and UNION ALL.sql
@@ -13,7 +13,10 @@ SELECT
     contactLastName
 FROM
     customers;
-    
+
+
+-- UNION vs UNION ALL
+
 CREATE TABLE t1 (
     id INT PRIMARY KEY
 );
@@ -25,13 +28,14 @@ CREATE TABLE t2 (
 INSERT INTO t1 VALUES (1),(2),(3);
 INSERT INTO t2 VALUES (2),(3),(4);
 
-
+-- UNION operator
 SELECT id
 FROM t1
 UNION
 SELECT id
 FROM t2;
 
+-- UNION ALL operator
 SELECT id
 FROM t1
 UNION ALL
